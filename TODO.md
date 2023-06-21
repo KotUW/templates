2f9127756e1f6cd78001dc039e79337a991737ef
kotuw <46974369+KotUW@users.noreply.github.com>
2023-06-22 04:04:33 +0530

    TODO: idea dump

---
+++ TODO.md
@@ -0,0 +1,3 @@
+TODO:
+  - [ ] Make this app. preferly in other repo. It should take gh repo as an arg
+    + [ ] Should support jinja or other template fomat. for files. see rust for eample. 
---
+++ rust/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "..Unfinished..."
+exit 3
+
+# get project name
+cargo new $project_name
+
+#move all things inside this to the new director
+
+#delete this script from dir
+
+#git commit!
